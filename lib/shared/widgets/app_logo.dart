1666ba725f68635b51b4f4daf3a0c74df8d75338
Cristian Ristoiu <cristian.ristoiu@outlook.com>
2026-07-26 23:29:35 +0300

    CHT0127 - Corectie: Elimina asseturile invalide din AppLogo

--- lib/shared/widgets/app_logo.dart
+++ lib/shared/widgets/app_logo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../core/theme/app_colors.dart';
+
 class AppLogo extends StatelessWidget {
   final double size;
   final bool showBackground;
@@ -9,32 +11,31 @@ class AppLogo extends StatelessWidget {
     super.key,
     this.size = 120,
     this.showBackground = false,
-  });
+  }) : assert(size > 0);
 
   @override
   Widget build(BuildContext context) {
-    if (showBackground) {
-      return Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          SvgPicture.asset(
-            'docs/theme/design/logo-background.svg',
-            width: size,
-            height: size,
-          ),
-          SvgPicture.asset(
-            'docs/theme/design/logo-foreground.svg',
-            width: size * 0.6,
-            height: size * 0.6,
-          ),
-        ],
-      );
+    final logo = SvgPicture.asset(
+      'assets/brand/logo.svg',
+      width: showBackground ? size * 0.64 : size,
+      height: showBackground ? size * 0.64 : size,
+      fit: BoxFit.contain,
+      semanticsLabel: 'Contacte Duplicate',
+    );
+
+    if (!showBackground) {
+      return logo;
     }
 
-    return SvgPicture.asset(
-      'assets/brand/logo.svg',
-      width: size,
-      height: size,
+    return SizedBox.square(
+      dimension: size,
+      child: DecoratedBox(
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: AppColors.brandGradient,
+        ),
+        child: Center(child: logo),
+      ),
     );
   }
 }
