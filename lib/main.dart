6c2403275e81787e48bc1f3638b52695dbd85959
Cristian Ristoiu <cristian.ristoiu@outlook.com>
2026-07-27 01:03:05 +0300

    CHT0172 - Inregistreaza serviciul de backup in aplicatie

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import 'app/contact_duplicate_app.dart';
+import 'core/backup/contact_backup_service.dart';
 import 'core/contacts/contacts_scan_service.dart';
 import 'core/theme/theme_provider.dart';
+import 'features/backup/backup_controller.dart';
 import 'features/dashboard/scan_controller.dart';
 
 void main() {
@@ -18,6 +20,11 @@ void main() {
         ),
         ChangeNotifierProvider<ScanController>(
           create: (_) => ScanController(NativeContactsScanService()),
+        ),
+        ChangeNotifierProvider<BackupController>(
+          create: (_) => BackupController(
+            EncryptedContactBackupService(),
+          ),
         ),
       ],
       child: const ContactDuplicateApp(),
