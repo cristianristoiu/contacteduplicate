2bae88923bb0ceebcf6e00b477042f9f8a52838f
Cristian Ristoiu <cristian.ristoiu@outlook.com>
2026-07-27 01:17:16 +0300

    CHT0182 - Activeaza protectia sistemului pentru backupuri

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:provider/single_child_widget.dart';
 
 import 'app/contact_duplicate_app.dart';
 import 'core/backup/contact_backup_service.dart';
+import 'core/backup/protected_contact_backup_service.dart';
 import 'core/contacts/contacts_scan_service.dart';
 import 'core/theme/theme_provider.dart';
 import 'features/backup/backup_controller.dart';
@@ -26,7 +27,9 @@ void main() {
         ChangeNotifierProvider<BackupController>(
           create: (_) {
             final controller = BackupController(
-              EncryptedContactBackupService(),
+              ProtectedContactBackupService(
+                delegate: EncryptedContactBackupService(),
+              ),
             );
             unawaited(controller.load());
             return controller;
