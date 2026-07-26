ea4f03df71e53cf616bb288b150dc0574fe071bf
Cristian Ristoiu <cristian.ristoiu@outlook.com>
2026-07-26 23:38:43 +0300

    CHT0142 - Persista tema selectata de utilizator

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum AppThemeMode {
   system,
@@ -18,7 +21,16 @@ enum AppThemeMode {
 }
 
 class ThemeProvider extends ChangeNotifier {
+  static const String _preferenceKey = 'theme_mode';
+
+  final SharedPreferencesAsync _preferences;
   AppThemeMode _mode = AppThemeMode.system;
+  bool _isDisposed = false;
+
+  ThemeProvider({SharedPreferencesAsync? preferences})
+      : _preferences = preferences ?? SharedPreferencesAsync() {
+    unawaited(_restoreMode());
+  }
 
   AppThemeMode get mode => _mode;
 
@@ -31,5 +43,29 @@ class ThemeProvider extends ChangeNotifier {
 
     _mode = mode;
     notifyListeners();
+    unawaited(_preferences.setString(_preferenceKey, mode.name));
+  }
+
+  Future<void> _restoreMode() async {
+    final storedMode = await _preferences.getString(_preferenceKey);
+    if (storedMode == null || _isDisposed) {
+      return;
+    }
+
+    final restoredMode = AppThemeMode.values.where(
+      (mode) => mode.name == storedMode,
+    );
+    if (restoredMode.isEmpty || _mode == restoredMode.first) {
+      return;
+    }
+
+    _mode = restoredMode.first;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _isDisposed = true;
+    super.dispose();
   }
 }
