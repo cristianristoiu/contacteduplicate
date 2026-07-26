3087465d040d616fd01b6b578e40ad87aa934f76
Cristian Ristoiu <cristian.ristoiu@outlook.com>
2026-07-26 23:41:47 +0300

    CHT0146 - Inregistreaza serviciul de scanare in aplicatie

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 import 'app/contact_duplicate_app.dart';
+import 'core/contacts/contacts_scan_service.dart';
 import 'core/theme/theme_provider.dart';
+import 'features/dashboard/scan_controller.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
-    ChangeNotifierProvider<ThemeProvider>(
-      create: (_) => ThemeProvider(),
+    MultiProvider(
+      providers: <SingleChildWidget>[
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (_) => ThemeProvider(),
+        ),
+        ChangeNotifierProvider<ScanController>(
+          create: (_) => ScanController(NativeContactsScanService()),
+        ),
+      ],
       child: const ContactDuplicateApp(),
     ),
   );
